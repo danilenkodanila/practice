0d55dd8b31f4db9d53a7cf5f11e902e5b2716284
danilenkodanila <danilenkodanilasergeevich@gmail.com>
2017-11-29 01:08:19 +1000

    first page 💩
    Без адаптивности и с кучей 💩

---
+++ index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Практикант ДВФУ</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+      <div class="grid-x header">
+        <div class="small-1 large-4 columns site-name">Практикант ДВФУ</div>
+        <div class="small-2 large-4 columns"></div>
+        <div class="small-1 large-4 columns logo">
+          <img src="image/fefu-logo.png" class="fefu-logo">
+        </div>
+      </div>
+      <div class="grid-x header-2">
+        <div class="small-1 large-2 columns vacancies">
+          <a href="#" class="bt-1">Вакансии</a>
+        </div>
+        <div class="small-1 large-2 columns employers">
+          <a href="#" class="bt-2">Работодатели</a>
+        </div>
+        <div class="small-0 large-4 columns"></div>
+        <div class="small-1 large-3 columns logo">
+          <div class="lk">
+            <img src="image/lk-logo.png" class="lk-logo">
+            <a href="#" class="link">Личный кабинет</a>
+          </div>
+        </div>
+      </div>
+
+    
+     <div class="grid-x search-row">
+      <div class="small-0 large-1 columns"></div>
+      <div class="small-10 large-10 columns">
+        <input class="search-icon" type="text" placeholder="Поиск">
+      </div>
+      <div class="small-0 large-1 columns"></div>
+    </div>
+
+    <div class="grid-x block">
+      <div class="small-1 large-1 columns"></div>
+      <div class="small-10 large-10 columns bd">
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold text-left">Заголовок</div>
+            <div class="bold text-right">Работодатель</div>
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            &emsp;&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non metus a quam dapibus ullamcorper non consequat ex. Aenean porta iaculis dui, et vestibulum magna ultricies a. Nulla id semper libero. In quis est non tellus pharetra imperdiet nec et risus. Nunc pretium auctor mi vitae fringilla. Proin porttitor faucibus justo, ac convallis purus euismod vitae. Donec non ipsum arcu. Proin consequat tortor nunc, sit amet viverra velit accumsan eu.
+            <br>&emsp;&emsp;Vivamus consectetur sapien at malesuada semper. Suspendisse potenti. Cras in scelerisque velit. Aliquam dignissim, justo nec maximus facilisis, arcu neque fermentum mi, consequat ultricies justo nulla sed enim. Ut in magna eget turpis maximus rhoncus. Vivamus aliquet et metus at sollicitudin. Phasellus id suscipit magna.
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Ориентировано на:</div> студентов группы ...
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Дата проведения:</div> с ... по ... 
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Место проведения:</div> ул. ..., ООО ....
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+      </div>
+      <div class="small-1 large-1 columns"></div>
+    </div>
+
+    <div class="grid-x block">
+      <div class="small-1 large-1 columns"></div>
+      <div class="small-10 large-10 columns bd">
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold text-left">Заголовок</div>
+            <div class="bold text-right">Работодатель</div>
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            &emsp;&emsp;А тут короткое описание
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Ориентировано на:</div> студентов группы ...
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Дата проведения:</div> с ... по ... 
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+        <div class="grid-x block-line">
+          <div class="small-1 large-1 columns"></div>
+          <div class="small-10 large-10 columns">
+            <div class="bold">Место проведения:</div> ул. ..., ООО ....
+          </div>
+          <div class="small-1 large-1 columns"></div>
+        </div>
+
+      </div>
+      <div class="small-1 large-1 columns"></div>
+    </div>
+ 
+                   
+    <div class="footer">
+      <div class="blue"></div>
+      <div class="grid-x white">
+        <div class="large-4 columns footer-text">
+          Контактная информация<br>
+          Адреса технической поддержки
+        </div>
+      </div>
+    </div>
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
