8b6334bb6b45cb217a7d2f07735504a55ed8f03e
Luna19 <nasty.ost@mail.ru>
2017-12-11 22:02:40 +1000

    БД_подключение

---
+++ bd.php
@@ -0,0 +1,14 @@
+
+<?php
+$dbhost = "localhost"; // Имя хоста БД
+$dbusername = "newuser"; // Пользователь БД
+$dbpass = "newuser"; // Пароль к базе
+$dbname = "practice"; // Имя базы
+
+$dbconnect = mysql_connect ($dbhost, $dbusername, $dbpass); 
+if (!$dbconnect) { echo ("Не могу подключиться к серверу базы данных!"); }
+
+if(@mysql_select_db($dbname)) {}
+else die ("Не могу подключиться к базе данных $dbname!");
+
+?>
