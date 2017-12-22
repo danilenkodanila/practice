d71f1c14c42fd78409cf2b5cf514bb541b6037fb
danilenkodanila <danilenkodanilasergeevich@gmail.com>
2017-12-23 01:12:06 +1000

    added page "employers-detail" ❌🆗⭕️
    Новая страничка

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
       <div class="blue"></div>
       <div class="grid-x white">
         <div class="small-10 small-offset-1 medium-10 medium-offset-1 large-10 large-offset-1 footer-text cell">
-          Контактная информация<br>
-          Адреса технической поддержки
+          Дмитрий Борисов <br>
+          Телефон: 8 (999) 614-64-11
         </div>
       </div>
     </div>';
