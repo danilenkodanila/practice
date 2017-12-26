833ca0c51a881e7756d9b665519279d6b7166f12
xsvrx <34093733+xsvrx@users.noreply.github.com>
2017-12-26 20:08:02 +1000

    Add files via upload

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
       <div class="grid-x white">
         <div class="small-10 small-offset-1 medium-10 medium-offset-1 large-10 large-offset-1 footer-text cell">
           Дмитрий Борисов <br>
-          Телефон: 8 (999) 614-64-11
+          Телефон: 02
         </div>
       </div>
     </div>';
