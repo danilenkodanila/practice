7f3832efc621700036acf2eab17f6416cd9c2eba
Luna19 <nasty.ost@mail.ru>
2018-01-09 18:40:37 +1000

    бд(без этого не синхр)

--- bd_PDO.php
+++ bd_PDO.php
@@ -1,8 +1,8 @@
 
 <?php
 
-		$user = "admin";
-		$pass = "76543210";
+		$user = "newuser";
+		$pass = "newuser";
 		//, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 		try {
 			$pdo = new PDO('mysql:host=localhost;dbname=practice;charset=utf8', $user, $pass);
